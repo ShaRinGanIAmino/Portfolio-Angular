adfe9e020b641741d0319e979355cf0c0f9b84ac
aminoschabbeh12345 <aminoschabbeh12345@gmail.com>
2024-08-16 16:46:37 +0100

    linking to github

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0E0E0E",
+        secondary: "#EAEAEA",
+        border: "#A3A3A3",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        mont: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
